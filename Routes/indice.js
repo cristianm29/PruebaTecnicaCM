a12461b8bf9fbd817af436fc925b3959d3120d9f
cristianm29 <cmachuca168@gmail.com>
2021-03-04 11:28:40 -0500

    Autenticacion y listar todos los productos

---
+++ Routes/indice.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const enrutar = express.Router();
+//const enrutar_productos = require ('../Functions/Productos');
+//const enrutar_cupon = require ('../Functions/Cupon');
+
+var Cargar_productos = [
+    {id: 1, name: 'Producto_1'},
+    {id: 2, name: 'Producto_2'},
+    {id: 3, name: 'Producto_3'},
+];
+
+var Cargar_cupon = [
+    {id: 1, name: 'Cupon_1', description: 'Este es el cupon 1', product_id: 2, valid_since: '2021-03-04T00:00:00.000', valid_until: '2021-03-04T23:59:00.999'},
+    {id: 2, name: 'Cupon_2', description: 'Este es el cupon 2', product_id: 3, valid_since: '2021-03-03T00:00:00.000', valid_until: '2021-03-03T23:59:00.999'},
+];
+
+function listar_pro (req, res) {
+    
+    res.status(200).json({
+
+        datos : Cargar_productos
+
+    });
+}
+
+function Auten_Admin (req, res, next){
+
+    let autA = req.headers.auta;
+
+    if (autA === 'Admin'){
+
+        next();
+
+    } else {
+
+        res.status(200).json({
+
+            message : 'Error 403'
+
+        });
+    }
+}
+
+function Auten_Customer (req, res, next){
+
+    let autC = req.headers.autc;
+
+    if (autC === 'Customer'){
+
+        next();
+
+    } else {
+
+        res.status(200).json({
+
+            message : 'Error 403'
+
+        });
+    }
+}
+
+enrutar.get('/productos', [Auten_Admin, listar_pro]);
+
+module.exports = enrutar;
--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const app_ser = express();
+const routes = require('./Routes/indice');
 
-//Server
+
+//Servidor
 app_ser.set('port', process.env.PORT || 3000);
+
+app_ser.use(routes);
+
+app_ser.use(function (req, res, next)  {
+    console.log(`${req.url} -${req.method}`);
+    next();
+});
+app_ser.use(body_parser.json());
+app_ser.use(body_parser.urlencoded({extended: false}));
 
 app_ser.listen(app_ser.get('port'), function() {
     console.log('servidor en el puerto', app_ser.get('port'))
 });
+
+
+
+//Rutas
+
+
+/*
+
+*/
